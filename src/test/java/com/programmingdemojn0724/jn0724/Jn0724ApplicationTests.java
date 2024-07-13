13b6447f1b0933530592e158c3c5c32f5de309ce
joelNj <joelngalamulume90@gmail.com>
2024-07-13 16:32:31 -0500

    latest version with test cases

--- src/test/java/com/programmingdemojn0724/jn0724/Jn0724ApplicationTests.java
+++ src/test/java/com/programmingdemojn0724/jn0724/Jn0724ApplicationTests.java
@@ -21,15 +21,4 @@ class Jn0724ApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	void getChargeDays(){
-		Customer customer = new Customer("Joel","Ngalamulume");
-		ToolType toolType1 = new ToolType( "Ladder", 1.99, true,false, true);
-		Tool tool2 = new Tool("LADW", toolType1, "Werner");
-		Rental rental = new Rental(customer,tool2, LocalDate.of(2020,7,5),3,10);
-		int expected = 3;
-		int actual =  rental.getChargeDays();
-		assertEquals(expected,actual,"the expected value is 2");
-	}
-
 }
